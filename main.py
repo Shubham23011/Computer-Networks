86c347e9559383e92fc9ae001d2dea6f96564bf2
Shubham <shubhamraihero2015@gmail.com>
2024-05-09 11:44:22 +0530

    CN Project Submission 1

---
+++ main.py
@@ -0,0 +1,12 @@
+from Hub import *
+from Physical_Layer import physicalLayer
+from Data_link_layer import Data_link_layer
+
+def main():
+    SD_Data, SourceDevice = physicalLayer()
+    print("*" * 60)
+    print("Entering into Data Link Layer part\n")
+    SD_Data = Data_link_layer(SD_Data, SourceDevice)
+
+if __name__ == "__main__":
+    main()
